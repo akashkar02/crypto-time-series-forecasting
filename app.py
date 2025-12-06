4e3c4da08a375fe21b182c3dbabea5e2e6217b85
akashkar02 <akash.karchoudhury@gmail.com>
2025-12-06 21:57:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.set_page_config(
 # --------------------------------------------------
 # LOAD DATA
 # --------------------------------------------------
-DATA_PATH = "binance_crypto_data_file.csv"
+data = pd.read_csv("binance_crypto_data_file.csv")
 
 @st.cache_data
 def load_data():
@@ -365,6 +365,7 @@ elif page == "Model Evaluation":
         st.dataframe(metrics_df)
     else:
         st.info("Please run models in 'Forecasting Models' first.")
+
 
 
 
